b52284a090c1c7214d103b0bb6b4fcbdd5d21ccd
Patrick <patrick.vuscan@gmail.com>
2020-10-31 15:56:35 -0400

    chore: added ESLint and nativebase

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'no-multi-spaces': ['error'],
+    'react/style-prop-object': ['off'],
+  },
+};
--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,67 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from '@react-navigation/native';
+import * as Font from 'expo-font';
 import React from 'react';
 import Drinks from "./screens/Drinks";
 import Orders from "./screens/Orders";
 import Pizzas from "./screens/Pizzas";
 import Toppings from "./screens/Toppings";
 
-
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+export default class App extends React.Component {
+  //checking state for if font is loaded or not.
+  state = {
+    fontLoaded: false,
+  };
+
+   async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    //Setting the state to true when font is loaded.
+    this.setState({ fontLoaded: true });
+   }
+
+  render() {
+    return (
+        <NavigationContainer>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+	>
+        <Tab.Screen name="Pizzas" component={Pizzas} />
+        <Tab.Screen name="Drinks" component={Drinks} />
+        <Tab.Screen name="Toppings" component={Toppings} />
+        <Tab.Screen name="Orders" component={Orders} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    );
+  }
+}
+
+/* export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(async () => {
+    !isReady && await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+
+      setIsReady(true);
+
+  }, [])
+
+  if (!isReady) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -25,4 +77,4 @@ export default function App() {
       </Tab.Navigator>
     </NavigationContainer>
   );
-}
+} */
