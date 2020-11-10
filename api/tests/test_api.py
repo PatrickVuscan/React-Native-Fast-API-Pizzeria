05be3247c1db5a207c2aa87e5b31aac9873a6220
mhd53 <mhd53@users.noreply.github.com>
2020-11-10 16:33:11 -0500

    test: add drink unit tests for API routes

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -8,66 +8,6 @@ from api.app import app
 client = TestClient(app)
 
 
-class ToppingTest(unittest.TestCase):
-    """Test all routes associated with topping."""
-
-    def test_read_topping(self):
-        res_top = client.post("/toppings", json={"name": "Pepperroni", "price": 3.98})
-        res_top = res_top.json()
-
-        res = client.get(f"/toppings/{res_top['topping_id']}")
-        assert res.status_code == 200
-        top = res.json()
-        assert top["name"] == "Pepperroni"
-        assert top["price"] == 3.98
-
-    def test_read_toppings(self):
-        res = client.get("/toppings")
-        assert res.status_code == 200
-        toppings = res.json()
-        assert toppings is not None
-
-    def test_create_topping(self):
-        res = client.post("/toppings", json={"name": "Pepperoni", "price": 3.99})
-        assert res.status_code == 200
-        toppings = res.json()
-        assert toppings is not None
-
-    def test_update_topping_name(self):
-        res = client.post("/toppings", json={"name": "Pepperoni", "price": 3.99})
-        topping = res.json()
-
-        res = client.put(f"/toppings/{topping['topping_id']}", json={"name": "Chicken"})
-        assert res.status_code == 200
-
-        top = res.json()
-        assert top["name"] == "Chicken"
-        assert top["price"] == 3.99
-
-    def test_update_topping_price(self):
-        res = client.post("/toppings", json={"name": "Pepperoni", "price": 3.99})
-        topping = res.json()
-
-        res = client.put(f"/toppings/{topping['topping_id']}", json={"price": 2.99})
-        assert res.status_code == 200
-
-        top = res.json()
-        assert top["name"] == "Pepperoni"
-        assert top["price"] == 2.99
-
-    def test_delete_topping(self):
-        res_top = client.post("/toppings", json={"name": "Pepperroni", "price": 3.98})
-        res_top = res_top.json()
-
-        res = client.delete(f"/toppings/{res_top['topping_id']}")
-        assert res.status_code == 200
-        del_top = res.json()
-        assert del_top["topping_id"] == res_top["topping_id"]
-
-        with pytest.raises(Exception):
-            client.get(f"/toppings/{res_top['topping_id']}")
-
-
 class PizzaTest(unittest.TestCase):
     """Test all routes associated with pizza."""
 
@@ -164,3 +104,142 @@ class PizzaTest(unittest.TestCase):
 
         with pytest.raises(Exception):
             client.get(f"/pizzas/{res_piz['pizza_id']}")
+
+
+class ToppingTest(unittest.TestCase):
+    """Test all routes associated with topping."""
+
+    def test_read_topping(self):
+        res_top = client.post("/toppings", json={"name": "Pepperroni", "price": 3.98})
+        res_top = res_top.json()
+
+        res = client.get(f"/toppings/{res_top['topping_id']}")
+        assert res.status_code == 200
+        top = res.json()
+        assert top["name"] == "Pepperroni"
+        assert top["price"] == 3.98
+
+    def test_read_toppings(self):
+        res = client.get("/toppings")
+        assert res.status_code == 200
+        toppings = res.json()
+        assert toppings is not None
+
+    def test_create_topping(self):
+        res = client.post("/toppings", json={"name": "Pepperoni", "price": 3.99})
+        assert res.status_code == 200
+        toppings = res.json()
+        assert toppings is not None
+
+    def test_update_topping_name(self):
+        res = client.post("/toppings", json={"name": "Pepperoni", "price": 3.99})
+        topping = res.json()
+
+        res = client.put(f"/toppings/{topping['topping_id']}", json={"name": "Chicken"})
+        assert res.status_code == 200
+
+        top = res.json()
+        assert top["name"] == "Chicken"
+        assert top["price"] == 3.99
+
+    def test_update_topping_price(self):
+        res = client.post("/toppings", json={"name": "Pepperoni", "price": 3.99})
+        topping = res.json()
+
+        res = client.put(f"/toppings/{topping['topping_id']}", json={"price": 2.99})
+        assert res.status_code == 200
+
+        top = res.json()
+        assert top["name"] == "Pepperoni"
+        assert top["price"] == 2.99
+
+    def test_delete_topping(self):
+        res_top = client.post("/toppings", json={"name": "Pepperroni", "price": 3.98})
+        res_top = res_top.json()
+
+        res = client.delete(f"/toppings/{res_top['topping_id']}")
+        assert res.status_code == 200
+        del_top = res.json()
+        assert del_top["topping_id"] == res_top["topping_id"]
+
+        with pytest.raises(Exception):
+            client.get(f"/toppings/{res_top['topping_id']}")
+
+
+class DrinkTest(unittest.TestCase):
+    """Test all routes associated with drinks."""
+
+    def test_create_drink(self):
+        res = client.post("/drinks", json={"name": 0, "price": 2.99})
+
+        assert res.status_code == 200
+
+        drink = res.json()
+        assert drink["name"] == 0
+        assert drink["price"] == 2.99
+
+    def test_read_drink(self):
+        res_drink = client.post("/drinks", json={"name": 0, "price": 2.99})
+        res_drink = res_drink.json()
+
+        res = client.get(f"drinks/{res_drink['drink_id']}")
+        drink = res.json()
+        assert drink["name"] == 0
+        assert drink["price"] == 2.99
+
+    def test_read_drinks(self):
+        res_d1 = client.post("/drinks", json={"name": 0, "price": 2.99})
+        res_d2 = client.post("/drinks", json={"name": 0, "price": 2.99})
+        res_d3 = client.post("/drinks", json={"name": 0, "price": 2.99})
+
+        drink1 = res_d1.json()
+        drink2 = res_d2.json()
+        drink3 = res_d3.json()
+
+        res = client.get("drinks")
+
+        assert res.status_code == 200
+
+        drinks = res.json()[-3:]
+        assert len(drinks) == 3
+        assert drinks[0]["drink_id"] == drink1["drink_id"]
+        assert drinks[1]["drink_id"] == drink2["drink_id"]
+        assert drinks[2]["drink_id"] == drink3["drink_id"]
+
+    def test_update_drink_name(self):
+        res_drink = client.post("/drinks", json={"name": 0, "price": 2.99})
+
+        drink = res_drink.json()
+
+        res = client.put(f"/drinks/{drink['drink_id']}", json={"name": 2})
+
+        assert res.status_code == 200
+
+        udrink = res.json()
+        assert udrink["name"] == 2
+        assert udrink["price"] == 2.99
+
+    def test_update_drink_price(self):
+        res_drink = client.post("/drinks", json={"name": 0, "price": 2.99})
+
+        drink = res_drink.json()
+
+        res = client.put(f"/drinks/{drink['drink_id']}", json={"price": 2.0})
+
+        assert res.status_code == 200
+
+        udrink = res.json()
+        assert udrink["name"] == 0
+        assert udrink["price"] == 2.0
+
+    def test_delete_drink(self):
+        res_drink = client.post("/drinks", json={"name": 0, "price": 2.99})
+        res_drink = res_drink.json()
+
+        res = client.delete(f"/drinks/{res_drink['drink_id']}")
+        assert res.status_code == 200
+        del_drink = res.json()
+        assert del_drink["drink_id"] == res_drink["drink_id"]
+
+        with pytest.raises(Exception):
+            client.get(f"/drinks/{res_drink['drink_id']}")
